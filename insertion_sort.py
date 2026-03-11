71379ceda969e117a395356667c4e72166ee1eaa
bartoszmalczyk <bartoszmalczyk@icloud.com>
2026-03-11 14:15:55 +0100

    insertion sort

---
+++ insertion_sort.py
@@ -0,0 +1,32 @@
+# [23, 1, 10, 5, 2]
+def insertionSort(nums):
+    n = len(nums)
+    for i in range(1,n):
+        key = nums[i]
+        j = i - 1
+        print(nums)
+        while j >= 0 and nums[j] > key:
+            nums[j + 1] = nums[j]
+            j -= 1
+        nums[j + 1] = key
+
+nums = [23, 1, 10, 5, 2]
+insertionSort(nums)
+print(nums)
+    
+
+
+"""
+psuedocode:
+
+def insertionSort(int nums[]):
+    m = nums.length()
+    for i in range(1, m):
+        key = nums[i]
+        j = i - 1
+        while j >= 0 and nums[j] > key:
+            nums[j + 1] = nums[j]
+            j -= 1
+        nums[j + 1] = key
+
+"""
